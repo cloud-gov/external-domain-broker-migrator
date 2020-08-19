d8443a82dad631a0c0e56c12c7bace2e9d3a5e0c
Ben Berry <ben.berry@gsa.gov>
2020-08-19 16:48:22 -0700

    add startup checks

---
+++ migrator/__main__.py
@@ -0,0 +1,13 @@
+import time
+
+from migrator.db import check_connections
+
+
+def main():
+    check_connections()
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
