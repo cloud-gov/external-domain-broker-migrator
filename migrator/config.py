7ec69719d271c097ffb258563d12eef6e584c646
Ben Berry <ben.berry@gsa.gov>
2020-08-24 16:22:48 -0700

    add aws config and client

--- migrator/config.py
+++ migrator/config.py
@@ -28,6 +28,9 @@ class LocalConfig(Config):
         self.CDN_BROKER_DATABASE_URI = "postgresql://localhost/local-development-cdn"
         self.DNS_VERIFICATION_SERVER = "127.0.0.1:8053"
         self.DNS_ROOT_DOMAIN = "domains.cloud.test"
+        self.AWS_COMMERCIAL_REGION = "us-west-1"
+        self.AWS_COMMERCIAL_ACCESS_KEY_ID = "ASIANOTAREALKEY"
+        self.AWS_COMMERCIAL_SECRET_ACCESS_KEY = "THIS_IS_A_FAKE_KEY"
 
 
 class AppConfig(Config):
@@ -37,6 +40,13 @@ class AppConfig(Config):
         self.CDN_BROKER_DATABASE_URI = cdn_db.credentials["uri"]
         self.DNS_VERIFICATION_SERVER = "8.8.8.8:53"
         self.DNS_ROOT_DOMAIN = self.env_parser("DNS_ROOT_DOMAIN")
+        self.AWS_COMMERCIAL_REGION = self.env_parser("AWS_COMMERCIAL_REGION")
+        self.AWS_COMMERCIAL_ACCESS_KEY_ID = self.env_parser(
+            "AWS_COMMERCIAL_ACCESS_KEY_ID"
+        )
+        self.AWS_COMMERCIAL_SECRET_ACCESS_KEY = self.env_parser(
+            "AWS_COMMERCIAL_SECRET_ACCESS_KEY"
+        )
 
 
 class DevelopmentConfig(AppConfig):
