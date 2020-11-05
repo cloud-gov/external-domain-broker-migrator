c7b0e784c4bd50af6fbb5e0e8ef55381d4323ccd
Ben Berry <ben.berry@gsa.gov>
2020-11-04 16:08:07 -0800

    remove external-domain-broker database references

--- migrator/db.py
+++ migrator/db.py
@@ -4,11 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from migrator.extensions import config
-from migrator.models import CdnBase, EdbBase
+from migrator.models import CdnBase
 
 cdn_engine = create_engine(config.CDN_BROKER_DATABASE_URI)
-external_domain_engine = create_engine(config.EXTERNAL_DOMAIN_BROKER_DATABASE_URI)
-Session = sessionmaker(binds={CdnBase: cdn_engine, EdbBase: external_domain_engine})
+Session = sessionmaker(binds={CdnBase: cdn_engine})
 
 
 @contextmanager
@@ -20,13 +19,7 @@ def session_handler():
         session.close()
 
 
-def check_connections(
-    session_maker=Session,
-    cdn_binding=cdn_engine,
-    external_domain_binding=external_domain_engine,
-):
+def check_connections(session_maker=Session, cdn_binding=cdn_engine):
     session = session_maker()
     session.execute("SELECT 1 FROM certificates", bind=cdn_binding)
-    session.close()
-    session.execute("SELECT 1 FROM service_instance", bind=external_domain_binding)
     session.close()
