6e597cae7329400e1dc5488953c3fc4df3162b04
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2021-02-11 16:25:42 -0700

    add TXT record lookup

--- migrator/dns.py
+++ migrator/dns.py
@@ -23,6 +23,21 @@ def get_cname(domain: str) -> str:
         return ""
 
 
+def get_txt(domain: str) -> list:
+    try:
+        answers = _resolver.resolve(domain, "TXT")
+        results = []
+        for answer in answers:
+            results.append(answer.to_text().strip('"'))
+        return results
+
+    except dns.resolver.NXDOMAIN:
+        return ""
+
+    except dns.resolver.NoAnswer:
+        return ""
+
+
 def acme_challenge_cname_target(domain: str) -> str:
     return f"_acme-challenge.{domain}.{_root_dns}"
 
--- tests/unit/test_dns.py
+++ tests/unit/test_dns.py
@@ -1,7 +1,14 @@
-from migrator.dns import get_cname
+from migrator.dns import get_cname, get_txt
 
 
-def test_dns_can_get_cname(dns):
+def test_dns_resolver_can_get_cname(dns):
     dns.add_cname("testcname.example.com.", "example.com")
     result = get_cname("testcname.example.com")
     assert result == "example.com"
+
+
+def test_dns_resolver_can_get_txt(dns):
+    dns.add_txt("testtxt.example.com.", "hello1")
+    dns.add_txt("testtxt.example.com.", "hello2")
+    results = get_txt("testtxt.example.com")
+    assert sorted(results) == sorted(["hello1", "hello2"])
