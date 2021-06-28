d6489ee45da4e746798ad55b7f0e634a46b0f50e
Ben Berry <ben.berry@gsa.gov>
2021-06-28 08:35:36 -0700

    Add initial full migration function
    Add end-to-end migration function and tests for it

--- tests/unit/test_migration.py
+++ tests/unit/test_migration.py
@@ -1280,3 +1280,347 @@ def test_migration_marks_route_migrated(clean_db, fake_cf_client):
     migration = Migration(route, clean_db, fake_cf_client)
     migration.mark_complete()
     assert route.state == "migrated"
+
+
+@pytest.mark.focus
+def test_migration_migrates_happy_path(
+    clean_db, fake_cf_client, iam_commercial, route53, cloudfront, dns, fake_requests
+):
+    """
+    Migrate should:
+    - enable the migration plan
+    - create a migration instance
+    - wait for the migration instance to be created
+    - call update on the migration instance
+    - wait for the update to complete
+    - mark the old instance migrated
+    - call purge on the old instance
+    - disable the migration plan
+    """
+    dns.add_cname("_acme-challenge.example.com")
+    dns.add_cname("_acme-challenge.foo.com")
+    route = CdnRoute()
+    route.state = "provisioned"
+    route.instance_id = "asdf-asdf"
+    route.domain_external = "example.com,foo.com"
+    migration = Migration(route, clean_db, fake_cf_client)
+
+    # load caches so we can slim this test down.
+    # We've already tested calls and lazy-loading elsewhere, so we can skip the mocks here
+    migration._space_id = "my-space-id"
+    migration._org_id = "my-org-id"
+    migration._cloudfront_distribution_data = {
+        "Id": "my-cloudfront-distribution",
+        "ARN": "aws:arn:cloudfront:my-cloudfront-distribution",
+        "DistributionConfig": {
+            "Origins": {
+                "Items": [
+                    {
+                        "Id": "my-custom-domain-id",
+                        "DomainName": "example.gov",
+                        "OriginPath": "example.gov",
+                        "S3OriginConfig": None,
+                        "CustomOriginConfig": {"OriginProtocolPolicy": "https-only"},
+                    }
+                ]
+            },
+            "DefaultCacheBehavior": {
+                "ForwardedValues": {
+                    "QueryString": False,
+                    "Cookies": {
+                        "Forward": "whitelist",
+                        "WhitelistedNames": {
+                            "Quantity": 1,
+                            "Items": ["white-listed-name"],
+                        },
+                    },
+                    "Headers": {"Quantity": 1, "Items": ["white-listed-name-header"]},
+                }
+            },
+            "CustomErrorResponses": {
+                "Quantity": 2,
+                "Items": [
+                    {
+                        "ErrorCode": 404,
+                        "ResponsePagePath": "/four-oh-four",
+                        "ResponseCode": "404",
+                        "ErrorCachingMinTTL": 300,
+                    },
+                    {
+                        "ErrorCode": 500,
+                        "ResponsePagePath": "/five-hundred",
+                        "ResponseCode": "500",
+                        "ErrorCachingMinTTL": 300,
+                    },
+                ],
+            },
+            "ViewerCertificate": {
+                "IAMCertificateId": "my-cloudfront-cert-id",
+                "ACMCertificateArn": "aws:arn:acm:my-cloudfront-cert",
+                "Certificate": "my-cloudfront-cert",
+            },
+        },
+    }
+    migration._iam_server_certificate_data = {
+        "Path": "/",
+        "ServerCertificateName": "my-server-cert",
+        "ServerCertificateId": "my-server-cert-id",
+        "Arn": "aws:arn:iam:my-server-cert",
+        "UploadDate": datetime.date(2021, 1, 1),
+        "Expiration": datetime.date(2022, 1, 1),
+    }
+
+    domains = ["example.gov", "foo.com"]
+    create_service_plan_visibility_response_body = """
+    {
+  "metadata": {
+    "guid": "new-plan-visibility-guid",
+    "url": "/v2/service_plan_visibilities/new-plan-visibility-guid",
+    "created_at": "2016-06-08T16:41:31Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "service_plan_guid": "foo",
+    "organization_guid": "bar",
+    "service_plan_url": "/v2/service_plans/foo",
+    "organization_url": "/v2/organizations/bar"
+  }
+}
+"""
+    fake_requests.post(
+        "http://localhost/v2/service_plan_visibilities",
+        text=create_service_plan_visibility_response_body,
+    )
+
+    create_service_instance_response_body = """
+{
+  "metadata": {
+    "guid": "my-migrator-instance",
+    "url": "/v2/service_instances/my-migrator-instance",
+    "created_at": "2016-06-08T16:41:29Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "external-domain-broker-migrator",
+    "credentials": {
+
+    },
+    "service_plan_guid": "739e78F5-a919-46ef-9193-1293cc086c17",
+    "space_guid": "my-space-guid",
+    "gateway_data": null,
+    "dashboard_url": null,
+    "type": "managed_service_instance",
+    "last_operation": {
+      "type": "create",
+      "state": "in progress",
+      "description": "",
+      "updated_at": "2016-06-08T16:41:26Z",
+      "created_at": "2016-06-08T16:41:29Z"
+    },
+    "space_url": "/v2/spaces/my-space-1-guid",
+    "service_plan_url": "/v2/service_plans/739e78F5-a919-46ef-9193-1293cc086c17",
+    "service_bindings_url": "/v2/service_instances/my-migrator-instance/service_bindings",
+    "service_keys_url": "/v2/service_instances/my-migrator-instance/service_keys",
+    "routes_url": "/v2/service_instances/my-migrator-instance/routes",
+    "shared_from_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_from",
+    "shared_to_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_to"
+  }
+}"""
+
+    fake_requests.post(
+        "http://localhost/v2/service_instances",
+        text=create_service_instance_response_body,
+    )
+
+    service_instance_create_check_response_body = """
+{
+  "metadata": {
+    "guid": "my-migrator-instance",
+    "url": "/v2/service_instances/my-migrator-instance",
+    "created_at": "2016-06-08T16:41:29Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "external-domain-broker-migrator",
+    "credentials": { },
+    "service_plan_guid": "739e78F5-a919-46ef-9193-1293cc086c17",
+    "space_guid": "my-space-guid",
+    "gateway_data": null,
+    "dashboard_url": null,
+    "type": "managed_service_instance",
+    "last_operation": {
+      "type": "create",
+      "state": "succeeded",
+      "description": "",
+      "updated_at": "2016-06-08T16:41:26Z",
+      "created_at": "2016-06-08T16:41:29Z"
+    },
+    "space_url": "/v2/spaces/my-space-guid",
+    "service_plan_url": "/v2/service_plans/739e78F5-a919-46ef-9193-1293cc086c17",
+    "service_bindings_url": "/v2/service_instances/my-migrator-instance/service_bindings",
+    "service_keys_url": "/v2/service_instances/my-migrator-instance/service_keys",
+    "routes_url": "/v2/service_instances/my-migrator-instance/routes",
+    "shared_from_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_from",
+    "shared_to_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_to"
+  }
+}"""
+
+    fake_requests.get(
+        "http://localhost/v2/service_instances/my-migrator-instance",
+        text=service_instance_create_check_response_body,
+    )
+    update_service_instance_response_body = """
+{
+  "metadata": {
+    "guid": "my-migrator-instance",
+    "url": "/v2/service_instances/my-migrator-instance",
+    "created_at": "2016-06-08T16:41:30Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "external-domain-broker-migrator",
+    "credentials": { },
+    "service_plan_guid": "739e78F5-a919-46ef-9193-1293cc086c17",
+    "space_guid": "my-space-guid",
+    "gateway_data": null,
+    "dashboard_url": null,
+    "type": "managed_service_instance",
+    "last_operation": {
+      "type": "update",
+      "state": "in progress",
+      "description": "",
+      "updated_at": "2016-06-08T16:41:30Z",
+      "created_at": "2016-06-08T16:41:30Z"
+    },
+    "tags": [ ],
+    "maintenance_info": {
+      "version": "2.1.0",
+      "description": "OS image update. Expect downtime."
+    },
+    "space_url": "/v2/spaces/my-space-guid",
+    "service_plan_url": "/v2/service_plans/739e78F5-a919-46ef-9193-1293cc086c17",
+    "service_bindings_url": "/v2/service_instances/my-migrator-instance/service_bindings",
+    "service_keys_url": "/v2/service_instances/my-migrator-instance/service_keys",
+    "routes_url": "/v2/service_instances/my-migrator-instance/routes",
+    "shared_from_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_from",
+    "shared_to_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_to"
+  }
+}"""
+
+    fake_requests.put(
+        "http://localhost/v2/service_instances/my-migrator-instance",
+        text=update_service_instance_response_body,
+    )
+    service_instance_update_check_response_body = """
+{
+  "metadata": {
+    "guid": "my-migrator-instance",
+    "url": "/v2/service_instances/my-migrator-instance",
+    "created_at": "2016-06-08T16:41:29Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "external-domain-broker-migrator",
+    "credentials": { },
+    "service_plan_guid": "739e78F5-a919-46ef-9193-1293cc086c17",
+    "space_guid": "my-space-guid",
+    "gateway_data": null,
+    "dashboard_url": null,
+    "type": "managed_service_instance",
+    "last_operation": {
+      "type": "update",
+      "state": "succeeded",
+      "description": "",
+      "updated_at": "2016-06-08T16:41:26Z",
+      "created_at": "2016-06-08T16:41:29Z"
+    },
+    "space_url": "/v2/spaces/my-space-guid",
+    "service_plan_url": "/v2/service_plans/739e78F5-a919-46ef-9193-1293cc086c17",
+    "service_bindings_url": "/v2/service_instances/my-migrator-instance/service_bindings",
+    "service_keys_url": "/v2/service_instances/my-migrator-instance/service_keys",
+    "routes_url": "/v2/service_instances/my-migrator-instance/routes",
+    "shared_from_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_from",
+    "shared_to_url": "/v2/service_instances/0d632575-bb06-4ea5-bb19-a451a9644d92/shared_to"
+  }
+}"""
+
+    fake_requests.get(
+        "http://localhost/v2/service_instances/my-migrator-instance",
+        text=service_instance_update_check_response_body,
+    )
+    service_visibilities_response = """
+{
+   "total_results": 1,
+   "total_pages": 1,
+   "prev_url": null,
+   "next_url": null,
+   "resources": [
+      {
+         "metadata": {
+            "guid": "my-service-plan-visibility",
+            "url": "/v2/service_plan_visibilities/my-service-plan-visibility",
+            "created_at": "2021-02-22T21:15:57Z",
+            "updated_at": "2021-02-22T21:15:57Z"
+         },
+         "entity": {
+            "service_plan_guid": "739e78F5-a919-46ef-9193-1293cc086c17",
+            "organization_guid": "my-org-guid",
+            "service_plan_url": "/v2/service_plans/739e78F5-a919-46ef-9193-1293cc086c17",
+            "organization_url": "/v2/organizations/my-org-guid"
+         }
+      }
+   ]
+}
+    """
+    fake_requests.get(
+        "http://localhost/v2/service_plan_visibilities?q=organization_guid:my-org-id&q=service_plan_guid:739e78F5-a919-46ef-9193-1293cc086c17",
+        text=service_visibilities_response,
+    )
+    response_body = ""
+    fake_requests.delete(
+        "http://localhost/v2/service_plan_visibilities/my-service-plan-visibility",
+        text=response_body,
+    )
+
+    migration.migrate()
+
+    assert fake_requests.called
+    history = [request.url for request in fake_requests.request_history]
+
+    assert (
+        fake_requests.request_history[2].url
+        == "http://localhost/v2/service_plan_visibilities"
+    )
+    assert fake_requests.request_history[2].method == "POST"
+
+    assert (
+        fake_requests.request_history[3].url == "http://localhost/v2/service_instances"
+    )
+    assert fake_requests.request_history[3].method == "POST"
+    assert (
+        fake_requests.request_history[4].url
+        == "http://localhost/v2/service_instances/my-migrator-instance"
+    )
+    assert fake_requests.request_history[4].method == "GET"
+    assert (
+        fake_requests.request_history[5].url
+        == "http://localhost/v2/service_instances/my-migrator-instance"
+    )
+    assert fake_requests.request_history[5].method == "PUT"
+    assert (
+        fake_requests.request_history[6].url
+        == "http://localhost/v2/service_instances/my-migrator-instance"
+    )
+    assert fake_requests.request_history[6].method == "GET"
+    assert (
+        fake_requests.request_history[7].url
+        == "http://localhost/v2/service_plan_visibilities?q=organization_guid%3Amy-org-id&q=service_plan_guid%3A739e78F5-a919-46ef-9193-1293cc086c17"
+    )
+    assert fake_requests.request_history[7].method == "GET"
+    assert (
+        fake_requests.request_history[8].url
+        == "http://localhost/v2/service_plan_visibilities/my-service-plan-visibility"
+    )
+    assert fake_requests.request_history[8].method == "DELETE"
+
+    assert route.state == "migrated"
